d912fe50aa3f45a1553dc95cf91c67ad11667fe1
Jeff Beers <jeffb92@outlook.com>
2022-11-17 19:21:38 -0500

    contact form validation

--- script.js
+++ script.js
@@ -9,7 +9,98 @@ hamburger.addEventListener('click', function () {
     mobile_menu.classList.toggle('is-open');
 });
 
+// CONTACT
 
+let nameError = document.getElementById('name-error');
+let phoneError = document.getElementById('phone-error');
+let emailError = document.getElementById('email-error');
+let messageError = document.getElementById('message-error');
+let submitError = document.getElementById('submit-error');
+
+function validateName(){
+  let name = document.getElementById('contact-name').value;
+
+    // if no values, then error
+    if(name.length == 0){
+      nameError.innerHTML = 'Name is required';
+      return false;
+    }
+    //if not 2 names with a space, then error. 
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+      nameError.innerHTML = 'Write full name';
+      return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+    return true;
+}
+
+function validatePhone(){
+  let phone = document.getElementById('contact-phone').value;
+
+  if(phone.length == 0){
+    phoneError.innerHTML = 'Phone No. is required';
+    return false;
+  }
+
+  if(phone.length !== 10){
+    phoneError.innerHTML = 'Phone No. should be 10 digits';
+    return false;
+  }
+
+  //should be digits 0-9 for 10 characters
+  if(!phone.match(/^[0-9]{10}$/)){ 
+    phoneError.innerHTML = 'phone No. should be digits only';
+    return false;
+  }
+
+  phoneError.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+  return true;
+}
+
+function validateEmail(){
+  let email = document.getElementById('contact-email').value;
+
+  if(email.length == 0){
+    emailError.innerHTML = 'Email is required'
+    return false;
+  }
+
+  //  contain letters, can contain special characters or numbers, contains an @, and after that 2-4 more letters
+  if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][A-Za-z]{2,4}$/)){
+    emailError.innerHTML = 'Email is invalid'
+    return false;
+  }
+
+  emailError.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+  return true;
+}
+
+function validateMessage(){
+  let message = document.getElementById('contact-message').value;
+  let required = 30;
+  let remaining = required - message.length;
+
+  if(remaining > 0){
+    messageError.innerHTML = remaining + " more characters required";
+    return false;
+  }
+
+  messageError.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+  return true;
+}
+
+function validateForm(){
+  if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+    submitError.style.display = 'block';
+    submitError.innerHTML = 'Please fix error to submit';
+    setTimeout(function(){submitError.style.display = 'none';}, 3000);
+    //after 3 seconds the message will time out and be hidden.
+    return false;
+  }
+}
+
+
+  
 //PHOTOS SLIDESHOW
 let i = 0; // 0-based index. start point.
 let images = ['imgGallery/olrb1.JPG', 
@@ -85,4 +176,3 @@ fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=U
   
   countdown();
   setInterval(countdown, 1000);
-  
